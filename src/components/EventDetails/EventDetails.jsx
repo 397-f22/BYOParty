341d090cb7f1a70704715260c6ef389b17f1fb7b
kabaniak <kabaniak@gmail.com>
2022-12-02 09:47:03 -0600

    fix dates

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -7,23 +7,23 @@ import AddEventModal from '../AddEventModal/AddEventForm';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 
-const EventDetails = ({details, eventId, needed}) => {
+const EventDetails = ({ details, eventId, needed }) => {
     const [user] = useAuthState();
     const uid = user?.uid ? user.uid : "1";
     const [openModal, setOpenModal] = useState(false)
 
-    const EditEventModal = ({setOpenModal, details}) => {
+    const EditEventModal = ({ setOpenModal, details }) => {
         const navigate = useNavigate();
         const [update, result] = useDbUpdate(`/events/${eventId}`);
         const uid = user?.uid ? user.uid : "1";
-    
+
         const cb = (e, update, navigate, user, close) => {
             e.preventDefault();
-        
+
             let title = document.getElementById('eventTitle').value;
             let host = document.getElementById('eventHost').value;
             let time = document.getElementById('eventTime').value;
-        
+
             var newEvent = {
                 details: {
                     host: host,
@@ -31,12 +31,12 @@ const EventDetails = ({details, eventId, needed}) => {
                     title: title,
                     hostId: user.uid
                 },
-  
+
             }
             update(newEvent)
             setOpenModal(false)
         };
-    
+
         return (
             <form>
                 <h1>Edit Event</h1>
@@ -56,17 +56,19 @@ const EventDetails = ({details, eventId, needed}) => {
             </form>
         )
     }
-    const date = new Date(details.time)
+    let date = new Date(details.time)
     // console.log(date.toUTCString());
+    date = new Date(date.toISOString())
 
     return (
-        <div className={details.hostId == uid? "event-wrapper-host" : "event-wrapper"}>
-            
+        <div className={details.hostId == uid ? "event-wrapper-host" : "event-wrapper"}>
+
             <h2>{details.title}</h2>
-            {details.hostId == uid? <h3>Hosted by you</h3> : <h3>Hosted by {details.host}</h3>}
-            <p>{date.toString()}</p>
+            {details.hostId == uid ? <h3>Hosted by you</h3> : <h3>Hosted by {details.host}</h3>}
+            <p>{date.toLocaleDateString()} at {date.getHours() > 12 ? date.getHours() - 12 : date.getHours()}
+                :{date.getMinutes()} {date.getHours() >= 12 && date.getHours() != 24 ? "PM" : "AM"}</p>
             <Modal open={openModal} close={() => setOpenModal(false)}>
-                <EditEventModal setOpenModal={setOpenModal} details={details}/>
+                <EditEventModal setOpenModal={setOpenModal} details={details} />
             </Modal>
             <button className="btn btn-primary" onClick={() => setOpenModal(true)}>Edit</button>
         </div>
--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,15 +1,16 @@
 import EventDetails from "../EventDetails/EventDetails";
 
 const EventList = ({uid, userData, data}) => {
+    console.log(userData)
     return (
         <div>
-            {Object.entries(userData.eventsAttended).map(([eventId, val]) => 
+            {userData.eventsAttended? Object.entries(userData.eventsAttended).map(([eventId, val]) => 
                 <nav>
                     
                     <a href={data[eventId].details.hostId == uid? `/host/${eventId}`: `/join/${eventId}`} className=" text-decoration-none">
                         <EventDetails details={data[eventId].details} />
                     </a>
-                </nav>)}
+                </nav>) : <></>}
         </div>
     )
 }
