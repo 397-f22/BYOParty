44a3f34d40c057561e036ec52a220d65785007b7
kabaniak <kabaniak@gmail.com>
2022-12-07 14:44:56 -0600

    finished test two

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,7 +4,7 @@ import { uId } from '../../App';
 
 const Item = ({name, data, setSelect, id, user}) => {
     return (
-        <button className={data.selected === user.uid? "item-card-selected" : "item-card"} onClick={()=>setSelect(id)}>
+        <button className={data.selected === user.uid? "item-card-selected" : "item-card"} onClick={()=>setSelect(id)} data-testId={"item-button-"+name}>
             <p key={"item-"+name} data-testId={"item-"+name}>{name} x {data.quantity} {data.units == null? data.units: null}</p>
         </button>
     )
--- src/components/ItemList/ItemList.Baniak.test.jsx
+++ src/components/ItemList/ItemList.Baniak.test.jsx
@@ -3,74 +3,77 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemList from './ItemList';
 
-describe('AddEventModal', () => {
-    const user = {uid: '1'};
-    const events = {
-        "1234": {
-          "details": {
-            "host": "Susan Saroza",
-            "hostId": 1,
-            "time": "2022-11-23T20:00",
-            "title": "Housewarming"
-          },
-          "needed": {
-            "forks": {
-              "quantity": 30,
-              "selected": false,
-              "units": "null"
-            },
-            "cups": {
-              "quantity": 20,
-              "selected": 2,
-              "units": "null"
-            },
-            "milk": {
-              "quantity": 2,
-              "selected": false,
-              "units": "gallons"
-            }
-          }
+describe('ItemList', () => {
+  const user = { uid: '1' };
+  const events = {
+    "1234": {
+      "details": {
+        "host": "Susan Saroza",
+        "hostId": 1,
+        "time": "2022-11-23T20:00",
+        "title": "Housewarming"
+      },
+      "needed": {
+        "forks": {
+          "quantity": 30,
+          "selected": false,
+          "units": "null"
         },
-      }
-    
-      const users = {
-        "1": {
-          "eventsAttended": {
-            "1234": true,
-          }
+        "cups": {
+          "quantity": 20,
+          "selected": 2,
+          "units": "null"
+        },
+        "milk": {
+          "quantity": 2,
+          "selected": false,
+          "units": "gallons"
         }
       }
-  
+    },
+  }
+
+  const users = {
+    "1": {
+      "eventsAttended": {
+        "1234": true,
+      }
+    }
+  }
+
   test('only sees items not selected or selected by user', async () => {
     await render(
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={ <ItemList initItems={events['1234'].needed} user={user}/>} />
-          </Routes>
-        </BrowserRouter>
-    );
-
-    await Object.entries(events['1234'].needed).map(([key, data], i) => {
-        if(data.selected != 1 && data.selected != false){
-            expect (screen.queryByTestId("item-"+key)).toBeNull();
-        }
-        else {
-            expect (screen.getByTestId("item-"+key)).toBeDefined();
-        }
-    })
-    /*
-    render(
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<AddEventModal />} />
+          <Route path="*" element={<ItemList initItems={events['1234'].needed} user={user} />} />
         </Routes>
       </BrowserRouter>
     );
 
-    const submitButton = screen.getByTestId("submit-button");
-    await fireEvent.click(submitButton);
-    expect(await screen.getByText('data not valid')).toBeDefined();
-    */
+    await Object.entries(events['1234'].needed).map(([key, data], i) => {
+      if (data.selected != 1 && data.selected != false) {
+        expect(screen.queryByTestId("item-" + key)).toBeNull();
+      }
+      else {
+        expect(screen.getByTestId("item-" + key)).toBeDefined();
+      }
+    })
+  });
+
+  test('clicking an item toggles if its selected or not', async () => {
+    await render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<ItemList initItems={events['1234'].needed} user={user} />} />
+        </Routes>
+      </BrowserRouter>
+    );
+
+    let exItem = await screen.getByTestId("item-button-milk");
+    expect(exItem.className).toBe("item-card");
+    await fireEvent.click(exItem);
+
+    expect(screen.getByTestId("item-button-milk").className).toBe("item-card-selected")
 
   });
 });
